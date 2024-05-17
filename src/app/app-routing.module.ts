3df874e7caada90a0e2876be98f33384a9758e06
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-05-18 00:17:58 +0300

    End Of Section 03 - [ Error Handling ]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { HomeComponent } from './home/home.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
+  {path: 'not-found', component: NotFoundComponent, title: 'Not Found'},
+  {path: 'server-error', component: ServerErrorComponent, title: 'Server Error'},
+  {path: 'test-error', component: TestErrorComponent, title: 'Test Error'},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
   {path: '**', redirectTo: '/shop', pathMatch: 'full'},
 ];
